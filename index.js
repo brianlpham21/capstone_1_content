225ac3225ea86c34a6157be441fddb2b21d450de
Brian Pham <brianlpham21@gmail.com>
2017-12-08 15:27:19 -0800

    styling update and added share section

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ function retrieveJSON(searchTerm, callback1, callback2) {
   const query2 = {
     part: 'snippet',
     key: 'AIzaSyDW6ZU42JlJmV6NVAvR6jruTCKK39wP9ng',
-    q: 'marvel ' + searchTerm
+    q: 'marvel ' + searchTerm,
+    maxResults: 4
   };
 
   $.getJSON(MARVEL_API, query1, callback1);
@@ -29,9 +30,10 @@ function displayMarvelData(data) {
 
     $('.main-unknown-section').prop('hidden', false);
     $('.main-results-section').prop('hidden', true);
-    $('.main-comics-section').prop('hidden', true);
     $('.main-events-section').prop('hidden', true);
+    $('.main-comics-section').prop('hidden', true);
     $('.main-videos-section').prop('hidden', true);
+    $('.main-share-section').prop('hidden', true);
     return;
   }
   else {
@@ -51,6 +53,7 @@ function displayMarvelData(data) {
       $('.main-events-section').prop('hidden', false);
       $('.main-videos-section').prop('hidden', false);
       $('.main-results-section').prop('hidden', false);
+      $('.main-share-section').prop('hidden', false);
     }
     else {
       $('.results-section').html(`
@@ -68,27 +71,8 @@ function displayMarvelData(data) {
       $('.main-events-section').prop('hidden', false);
       $('.main-videos-section').prop('hidden', false);
       $('.main-results-section').prop('hidden', false);
+      $('.main-share-section').prop('hidden', false);
     }
-
-    const query_comics = {
-      apikey: 'b9387eb3d701ea1e371e1f554eb585c5',
-      ts: '1',
-      hash: 'c516f34ed1b8c272e76721b1be1dfe71',
-      limit: 3
-    };
-
-    $.getJSON(MARVEL_COMICS_API + data.data.results[0].id + '/comics', query_comics, function(data) {
-      const results = data.data.results.map((item, index) => {
-        return `
-          <div class='comic-result'>
-            <a href='${item.urls[0].url}' target='_blank'><img src='${item.thumbnail.path + '.' + item.thumbnail.extension}' class='image' alt='comic-photo'></a>
-            <h4 class='item-title'>${item.title}</h4>
-            <p class='description'>${item.description}</p>
-          </div>
-        `});
-
-      $('.comics-section').html(results);
-    });
 
     const query_events = {
       apikey: 'b9387eb3d701ea1e371e1f554eb585c5',
@@ -109,6 +93,40 @@ function displayMarvelData(data) {
 
       $('.events-section').html(results);
     });
+
+
+    const query_comics = {
+      apikey: 'b9387eb3d701ea1e371e1f554eb585c5',
+      ts: '1',
+      hash: 'c516f34ed1b8c272e76721b1be1dfe71',
+      limit: 3
+    };
+
+    $.getJSON(MARVEL_COMICS_API + data.data.results[0].id + '/comics', query_comics, function(data) {
+      const results = data.data.results.map((item, index) => {
+        console.log(item.description);
+        if (item.description === null) {
+          return `
+            <div class='comic-result'>
+              <a href='${item.urls[0].url}' target='_blank'><img src='${item.thumbnail.path + '.' + item.thumbnail.extension}' class='image' alt='comic-photo'></a>
+              <h4 class='item-title'>${item.title}</h4>
+              <p class='no-description'>No description available.</p>
+            </div>
+          `
+        }
+        else {
+          return `
+            <div class='comic-result'>
+              <a href='${item.urls[0].url}' target='_blank'><img src='${item.thumbnail.path + '.' + item.thumbnail.extension}' class='image' alt='comic-photo'></a>
+              <h4 class='item-title'>${item.title}</h4>
+              <p class='description'>${item.description}</p>
+            </div>
+          `;
+        }
+      });
+
+      $('.comics-section').html(results);
+    });
   }
 }
 
@@ -116,12 +134,12 @@ function displayYouTubeData(data) {
   const results = data.items.map((item, index) => {
     return `
       <div class='video-result'>
-        <h4 class='item-title'>${item.snippet.title}</h4>
-        <p>Channel:
+        <a href='#' class='video-box' id='${item.id.videoId}'><img src='${item.snippet.thumbnails.medium.url}' class='video' alt='video-image'></a>
+        <h4 class='video-title'>${item.snippet.title}</h4>
+        <p class='video-channel'>Channel:
           <a href='https://www.youtube.com/channel/${item.snippet.channelId}' target='_blank'>${item.snippet.channelTitle}</a>
         </p>
         <p class='video-description'>${item.snippet.description}</p>
-        <a href='#' class='video' id='${item.id.videoId}'><img src='${item.snippet.thumbnails.medium.url}'></a>
       </div>
     `;
   });
@@ -153,41 +171,41 @@ $(watchLogo);
 
 // LightBox Features
 
-// function watchImageClick() {
-//   $('.videos-section').on('click', '.video', function() {
-//     $('.light-box-area').prop('hidden', false);
-//
-//     let number = $(this).attr('id');
-//     let link = 'https://www.youtube.com/embed/' + number;
-//
-//     if ($('#light-box').length <= 0) {
-//       $('.light-box-area').append(`
-//         <div id='light-box'>
-//           <span class='close-button'>close</span>
-//           <iframe width="1425" height="641" src='' frameborder="0" gesture="media" allowfullscreen></iframe>
-//         </div>`);
-//
-//       $('#light-box').show();
-//       $('#light-box iframe').attr('src', link);
-//     }
-//     else {
-//       $('#light-box').show();
-//       $('iframe').attr('src', link);
-//     }
-//   });
-// }
-//
-// function watchCloseClick() {
-//   $('.light-box-area').on('click', '.close-button', function() {
-//     $('.light-box-area').prop('hidden', true);
-//     $('#light-box').hide();
-//     $('iframe').attr('src', '');
-//   });
-// }
-//
-// function addLightBoxFeatures() {
-//   watchImageClick();
-//   watchCloseClick();
-// }
-//
-// $(addLightBoxFeatures);
+function watchImageClick() {
+  $('.videos-section').on('click', '.video-box', function() {
+    $('.light-box-area').prop('hidden', false);
+
+    let number = $(this).attr('id');
+    let link = 'https://www.youtube.com/embed/' + number;
+
+    if ($('#light-box').length <= 0) {
+      $('.light-box-area').append(`
+        <div id='light-box'>
+          <span class='close-button'>close</span>
+          <iframe width="1425" height="641" src='' frameborder="0" gesture="media" allowfullscreen></iframe>
+        </div>`);
+
+      $('#light-box').show();
+      $('iframe').attr('src', link);
+    }
+    else {
+      $('#light-box').show();
+      $('iframe').attr('src', link);
+    }
+  });
+}
+
+function watchCloseClick() {
+  $('.light-box-area').on('click', '.close-button', function() {
+    $('.light-box-area').prop('hidden', true);
+    $('#light-box').hide();
+    $('iframe').attr('src', '');
+  });
+}
+
+function addLightBoxFeatures() {
+  watchImageClick();
+  watchCloseClick();
+}
+
+$(addLightBoxFeatures);
